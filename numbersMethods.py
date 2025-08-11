48836ae04303ac3be0225da85d15a03c07b3aaa1
Asfar Waheed <asfarwaheedch123@gmail.com>
2025-08-11 14:56:34 +0500

    numbers

---
+++ numbersMethods.py
@@ -0,0 +1,23 @@
+import math
+
+x = 1
+y = 1.1
+z = 1 + 2j  # complex numeber
+
+# arithemetic operations
+print(x+y)  # addition
+print(x-y)  # subtraction
+print(x*y)  # multiplication
+print(x/y)  # division
+print(x**y)  # exponentiation
+print(x//y)  # floor division
+print(x % y)  # modulus
+
+x += 6  # increment x by 6
+
+print(round(2.22, 1))
+print(abs(-2.22))  # absolute value
+print(divmod(10, 3))  # quotient and remainder
+print(pow(2, 3))  # power function
+
+print(math.ceil(2.1))  # round up
---
+++ stringMethods.py
@@ -0,0 +1,13 @@
+name = "Asfar Waheed"
+print(name.upper())  # returns a new string so the original string is not altered
+print(name.lower())
+print(name.title())  # Capitalizes the first letter of each word
+print(name.strip())  # Removes leading and trailing whitespace
+print(name.lstrip())  # Removes leading whitespace
+print(name.rstrip())  # Removes trailing whitespace
+print(name.find("Asf"))
+# Replaces a substring with another substring
+print(name.replace("Asfar", "Asf"))
+print(name)
+print("As" in name)  # Checks if a substring exists in the string
+print("new" not in name)  # Checks if a substring does not exist in the string
