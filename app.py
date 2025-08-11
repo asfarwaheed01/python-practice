7ecc06d2245e1f68c5a616fe4903231669571d88
Asfar Waheed <asfarwaheedch123@gmail.com>
2025-08-11 14:35:32 +0500

    initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+# name = "Asfar"
+# print(len(name))
+# print(name[-1])
+# print(name[0:])
+
+first = "Asfar"
+second = "Waheed"
+full = f"{first} {second}"
+print(full)
+# print(first + " " + second)
