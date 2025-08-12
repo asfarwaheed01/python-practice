6af027164f5cdb283c055a5b178fff1caddd20ae
Asfar Waheed <asfarwaheedch123@gmail.com>
2025-08-12 12:25:19 +0500

    logical Operators

---
+++ logicalOperators.py
@@ -0,0 +1,9 @@
+high_income = False
+good_credit = True
+student = False
+
+
+if (high_income and good_credit) and not student:
+    print("Eligible for loan")
+else:
+    print("Not eligible for loan")
